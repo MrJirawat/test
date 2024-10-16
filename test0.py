7958822af97cd44eed50a8e6a699ec326c44565f
Mr.Jirawat <s67319010004@kktech.ac.th>
2024-10-16 13:47:43 +0700

    16/10/67

---
+++ test0.py
@@ -0,0 +1,10 @@
+print('hello world')
+
+# แนะนำตัวเอง
+name = "wat"
+age = "18"
+hobby = "อ่านนิยาย"
+
+print(f"สวัสดีครับ/ค่ะ, ฉันชื่อ {name}.")
+print(f"ฉันอายุ {age} ปี.")
+print(f"งานอดิเรกของฉันคือ {hobby}.")
